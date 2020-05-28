ba11a1c641f0695dd5884ab95cc76d658235de36
Rafal Jakubczyk <jakubczykrafal06@gmail.com>
2020-05-28 17:12:43 +0200

    better infoframe

--- Noise_reductor/initFrame.cpp
+++ Noise_reductor/initFrame.cpp
@@ -178,7 +178,7 @@ void InitFrame::add_frag(wxMouseEvent& event)
 
 void InitFrame::openFrames( wxCommandEvent& event )
 {
-// TODO: Implement openFrames
+
 	wxFrame* infoFrame = new InfoFrame(this, m_imageHandler);
 	wxFrame* editFrame = new EditFrame(this, m_imageHandler);
 	editFrame->Show();
